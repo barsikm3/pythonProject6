d72ce0ccad4d5c145491dd8a08e922be213a6583
barsikm3 <105535694+barsikm3@users.noreply.github.com>
2023-01-31 17:43:46 +0300

    1) Encoding - done 2) Html - done 3) Image attacments - done 4) Txt files - done 5) Word, Excel, PDF, PPPX - Done

--- emailinh smtlib ssl.py
+++ emailinh smtlib ssl.py
@@ -1,6 +1,5 @@
 import smtplib
 import ssl
-from os.path import basename
 from email.mime.text import MIMEText
 from email.mime.multipart import MIMEMultipart
 from email.mime.application import MIMEApplication
@@ -9,20 +8,20 @@ from email.mime.application import MIMEApplication
 port = 587  # For starttls
 smtp_server = "smtp.lesta.group"
 sender_email = "s_pasiukevich@lesta.group"
-receiver_email = "i_hauryliuk@lesta.group"
+receiver_email = "a_prakudzin@lesta.group"
 password = input("Type your password and press enter: ")
 msg = MIMEMultipart()
 
-# msg.attach(MIMEText("Ты считаешь, что если я холодная - я безчувственная и невкусная? Все, Дюпончик, вражда навеки "))
+# msg.attach(MIMEText(" как опшен для не для HTML "))
 # with open("New Text Document.txt", "rb") as f:
-    # file = MIMEApplication(f.read(), Name=basename("New Text Document.txt"))
+    # file = MIMEApplication(f.read(), Name=basename("New Text Document.txt"))  это вариант для добавление txt файлов
 # msg.attach(file)
-text = """"\
-    "Чыки бамбони " \
-       "Напоминаем, что в последний день в Минском офисе дд/мм/гггг вам нужно подойти туда " \
-       "в Волну в 925 кабинет (Staff Records) " \
-       "и вот сюда ас велл " \
-       """
+# text = """"\
+   # "Чыки бамбони " \
+    #   "Напоминаем, что в последний день в Минском офисе дд/мм/гггг вам нужно подойти туда " \
+     #  "в Волну в 925 кабинет (Staff Records) " \
+     #  "и вот сюда ас велл " \
+      # """
 
 
 html = """\
@@ -39,16 +38,26 @@ html = """\
 """
 
 msg['Subject'] = "Корпоративная приблуда"
-msg['From'] = "Решил срулить с галеры? Аста ла виста, детка"
+msg['From'] = "Excel, Word, PPX, PDF приаттачились"
 
-part1 = MIMEText(text, 'plain')
+part1 = MIMEApplication(open('ExTest.xlsx', 'rb').read())
+part1.add_header('Content-Disposition', 'attachment', filename='ExTest.xlsx')
 part2 = MIMEText(html, 'html')
 part3 = MIMEApplication(open('scale_1200.jpeg', 'rb').read())
 part3.add_header('Content-Disposition', 'attachment', filename='scale_1200.jpeg')
+part4 = MIMEApplication(open('Тестовый документы Word.docx', 'rb').read())
+part4.add_header('Content-Disposition', 'attachment', filename='Тестовый документы Word.docx')
+part5 = MIMEApplication(open('bel.pdf', 'rb').read())
+part5.add_header('Content-Disposition', 'attachment', filename='bel.pdf')
+part6 = MIMEApplication(open('Presentation1.pptx', 'rb').read())
+part6.add_header('Content-Disposition', 'attachment', filename='Presentation1.pptx')
 
 msg.attach(part3)
 msg.attach(part2)
 msg.attach(part1)
+msg.attach(part4)
+msg.attach(part5)
+msg.attach(part6)
 
 context = ssl.create_default_context()
 with smtplib.SMTP(smtp_server, port) as server:
