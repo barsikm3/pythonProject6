9b580936d132306868cc4ba970aa76da5e57dc9e
barsikm3 <105535694+barsikm3@users.noreply.github.com>
2023-02-02 16:35:59 +0300

    Update 48.py

--- 48.py
+++ 48.py
@@ -16,3 +16,22 @@ With ws
 End With
 
 End Sub
+
+Sub InsertDataIntoFilteredRows()
+
+Dim ws As Worksheet
+Set ws = ActiveSheet
+
+With ws
+    Dim visibleRange As Range
+    Set visibleRange = .Range("A1", .Cells(.Rows.Count, 1).End(xlUp)).SpecialCells(xlCellTypeVisible)
+    
+    Dim cell As Range
+    For Each cell In visibleRange
+        cell.Value = "Inserted Data"
+    Next cell
+End With
+
+End Sub
+
+
