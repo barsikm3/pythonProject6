9550f78165da8324d5189c1569372f3b31c76401
barsikm3 <105535694+barsikm3@users.noreply.github.com>
2023-02-03 15:14:49 +0300

    Update 49.py

--- 49.py
+++ 49.py
@@ -1,1 +1,33 @@
-def
+import tkinter as tk
+import tkinter.ttk as ttk
+import pandas as pd
+
+def search(*args):
+    input = select_data.get()
+    if len(input) < 2:
+        results_list.delete(0, tk.END)
+        return
+    matching = [email for email in emails if input in email]
+    results_list.delete(0, tk.END)
+    for email in matching:
+        results_list.insert(tk.END, email)
+
+def select_email(*args):
+    selection = results_list.get(results_list.curselection())
+    select_data.set(selection)
+
+root = tk.Tk()
+root.title("Autocomplete Search")
+
+df = pd.read_excel("emails.xlsx")
+emails = df["Email"].tolist()
+
+select_data = ttk.Combobox(root, values=emails, state="normal")
+select_data.bind("<KeyRelease>", search)
+select_data.pack(pady=10)
+
+results_list = tk.Listbox(root, height=5)
+results_list.pack(pady=10)
+results_list.bind("<Button-1>", select_email)
+
+root.mainloop()
