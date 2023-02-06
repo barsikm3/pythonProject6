0a29f0c682f7b70d2df4b04200acd39c551c02b1
barsikm3 <105535694+barsikm3@users.noreply.github.com>
2023-02-06 14:05:23 +0300

    test24.8

--- 52.py
+++ 52.py
@@ -1,46 +1,50 @@
 import tkinter as tk
-import tkinter.filedialog
+import tkinter.ttk as ttk
 import pandas as pd
-from tkinter import ttk
+from tkinter import filedialog
 
-def select_file():
-    file_path = tk.filedialog.askopenfilename(filetypes=[("Excel Files", "*.xlsx")])
-    if file_path:
-        df = pd.read_excel(file_path)
-        column_names = list(df.columns)
-        column_var.set(column_names[0])
-        column_options['values'] = column_names
 
-def update_results(search_value):
-    selected_column = column_var.get()
-    results = df[df[selected_column].str.contains(search_value, case=False)].loc[:, selected_column].tolist()
+def search(*args):
+    input = select_data.get()
+    if len(input) < 2:
+        results_list.delete(0, tk.END)
+        return
+    matching = [email for email in df[selected_column] if input in email]
     results_list.delete(0, tk.END)
-    for result in results[:5]:
-        results_list.insert(tk.END, result)
+    for email in matching:
+        results_list.insert(tk.END, email)
 
-def on_result_click(event):
-    result = results_list.get(results_list.curselection()[0])
-    combo.set(result)
-    results_list.pack_forget()
+
+def select_email(*args):
+    selection = results_list.get(results_list.curselection())
+    select_data.set(selection)
+
+
+def load_file(*args):
+    global df, selected_column
+    file_path = filedialog.askopenfilename()
+    df = pd.read_excel(file_path)
+    columns = df.columns.tolist()
+    column_dropdown.config(values=columns)
+    selected_column = columns[0]
+
 
 root = tk.Tk()
-root.title("Excel Autocomplete Search")
+root.title("Autocomplete Search")
 
-file_select_button = tk.Button(root, text="Select Excel File", command=select_file)
-file_select_button.pack()
-
-column_var = tk.StringVar()
-column_options = ttk.Combobox(root, textvariable=column_var, state="readonly")
-column_options.pack()
-
-combo = ttk.Combobox(root, values=[], height=5)
-combo.pack()
+select_data = ttk.Combobox(root, state="normal")
+select_data.bind("<KeyRelease>", search)
+select_data.pack(pady=10)
 
 results_list = tk.Listbox(root, height=5)
-results_list.pack_forget()
-results_list.bind("<<ListboxSelect>>", on_result_click)
+results_list.pack(pady=10)
+results_list.bind("<Button-1>", select_email)
 
-combo.bind("<<ComboboxSelected>>", lambda event: results_list.pack_forget())
-combo.bind("<KeyRelease>", lambda event: update_results(combo.get()))
+load_button = tk.Button(root, text="Load Excel File", command=load_file)
+load_button.pack(pady=10)
+
+column_dropdown = ttk.Combobox(root, state="disabled")
+column_dropdown.pack(pady=10)
+column_dropdown.bind("<<ComboboxSelected>>", lambda e: setattr(df, "selected_column", column_dropdown.get()))
 
 root.mainloop()
