a11c367969d7253b969add5dd03f6c4f8c7bc93c
barsikm3 <105535694+barsikm3@users.noreply.github.com>
2023-02-06 13:36:08 +0300

    Update 24.py

--- 24.py
+++ 24.py
@@ -1,31 +1,37 @@
 import tkinter as tk
+import tkinter.filedialog
 import tkinter.ttk as ttk
 import pandas as pd
-import os
-from tkinter import filedialog
 
 def load_file():
-    global df, emails
-    file_path = filedialog.askopenfilename()
-    df = pd.read_excel(file_path)
-    column_names = df.columns
-    if 'Email' in column_names:
-        emails = df['Email'].tolist()
-    else:
-        tk.messagebox.showerror("Error", "Email column not found in the excel file.")
+    file_path = tkinter.filedialog.askopenfilename(defaultextension=".xlsx", filetypes=[("Excel files", "*.xlsx")])
+    if file_path:
+        df = pd.read_excel(file_path)
+        sheet_names = df.keys()
+        sheet_var.set(sheet_names[0])
+        update_column_names(df[sheet_names[0]])
+
+def update_column_names(df):
+    column_names = list(df.columns)
+    column_var.set(column_names[0])
+    column_names_menu = column_names_dropdown["menu"]
+    column_names_menu.delete(0, "end")
+    for name in column_names:
+        column_names_menu.add_command(label=name, command=lambda value=name: column_var.set(value))
+
+def update_emails(df, column_name):
+    global emails
+    emails = df[column_name].tolist()
 
 def search(*args):
     input = select_data.get()
     if len(input) < 2:
         results_list.delete(0, tk.END)
         return
-    if 'emails' in globals():
-        matching = [email for email in emails if input in email]
-        results_list.delete(0, tk.END)
-        for email in matching:
-            results_list.insert(tk.END, email)
-    else:
-        tk.messagebox.showerror("Error", "Please load an excel file with Email column.")
+    matching = [email for email in emails if input in email]
+    results_list.delete(0, tk.END)
+    for email in matching:
+        results_list.insert(tk.END, email)
 
 def select_email(*args):
     selection = results_list.get(results_list.curselection())
@@ -34,10 +40,18 @@ def select_email(*args):
 root = tk.Tk()
 root.title("Autocomplete Search")
 
-load_file_btn = tk.Button(root, text="Load Excel File", command=load_file)
-load_file_btn.pack(pady=10)
+load_button = tk.Button(root, text="Load File", command=load_file)
+load_button.pack(pady=10)
 
-select_data = ttk.Combobox(root, state="normal")
+sheet_var = tk.StringVar()
+sheet_dropdown = ttk.OptionMenu(root, sheet_var, [], command=lambda sheet: update_column_names(df[sheet]))
+sheet_dropdown.pack(pady=10)
+
+column_var = tk.StringVar()
+column_names_dropdown = ttk.OptionMenu(root, column_var, [], command=lambda column: update_emails(df[sheet_var.get()], column))
+column_names_dropdown.pack(pady=10)
+
+select_data = ttk.Combobox(root, values=[], state="normal")
 select_data.bind("<KeyRelease>", search)
 select_data.pack(pady=10)
 
@@ -46,4 +60,5 @@ results_list.pack(pady=10)
 results_list.bind("<Button-1>", select_email)
 
 root.mainloop()
+
 
