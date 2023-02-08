ba6e5cccb6c3a1872acc8eaedf72250cffd7289f
barsikm3 <105535694+barsikm3@users.noreply.github.com>
2023-02-08 16:19:11 +0300

    Update Lists.py

--- Lists.py
+++ Lists.py
@@ -1,2 +1,18 @@
-li
+import tkinter as tk
+
+def create_new_list():
+    selected_value = combobox.get()
+    new_list = [selected_value] * 5
+    print(new_list)
+
+root = tk.Tk()
+
+combobox = tk.ttk.Combobox(root, values=[1, 2, 3, 4, 5])
+combobox.pack()
+
+create_new_list_button = tk.Button(root, text="Create New List", command=create_new_list)
+create_new_list_button.pack()
+
+root.mainloop()
+
 
